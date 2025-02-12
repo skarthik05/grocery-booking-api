16b048ea09273a027e5516b2238c2cc60483d2ef
karthik <karthiks0598@gmail.com>
2025-02-12 18:23:26 +0530

    revise main.ts to incorporate swagger, logger, interceptor and exception handling

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,39 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ENV_KEYS } from './constants/env.constants';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ApiResponseInterceptor } from './common/response.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { CustomLoggerService } from './common/logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
+  const logger = new CustomLoggerService();
+  const app = await NestFactory.create(AppModule, {
+    logger,
+  });
 
+  const configService = app.get(ConfigService);
   const port = configService.get<number>(ENV_KEYS.PORT, 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ApiResponseInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Grocery API')
+    .setDescription('API for managing groceries')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 }
 bootstrap();
