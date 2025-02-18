b392d3b3106618f132d9caeedf68d397121b804a
karthik <karthiks0598@gmail.com>
2025-02-18 19:37:56 +0530

    throw error if there is no user present in request object

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -1,9 +1,12 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { ITokenPayload } from 'src/auth/interface/token-payload.interface';
 export const CurrentUser = createParamDecorator(
   (data: keyof ITokenPayload, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     const user = request.user as ITokenPayload;
+    if (!user) {
+      throw new UnauthorizedException('Please login to continue');
+    }
     return data ? user?.[data] : user;
   },
 );
